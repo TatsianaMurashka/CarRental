f87d566115bb2a7bc83c25e1bfa1d7937b856968
TatsianaMurashka <murashkotatsiana@gmail.com>
2020-05-02 15:20:23 +0300

    Functional decomposing

--- src/main/java/com/htp/Main.java
+++ src/main/java/com/htp/Main.java
@@ -2,6 +2,10 @@ package com.htp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main");
+        System.out.println(generateWelcomePhrase());
+    }
+
+    private static String generateWelcomePhrase() {
+        return "My main";
     }
 }
