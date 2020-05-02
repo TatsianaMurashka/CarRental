fdeb3f4b608371c696907032b02cb7fa165d356b
TatsianaMurashka <murashkotatsiana@gmail.com>
2020-05-02 15:59:53 +0300

    Generating int value

--- src/main/java/com/htp/Main.java
+++ src/main/java/com/htp/Main.java
@@ -1,9 +1,16 @@
 package com.htp;
 
+import org.apache.commons.lang3.RandomUtils;
+
 public class Main {
     public static void main(String[] args) {
-      
+
         System.out.println(generateWelcomePhrase());
+        System.out.println(generateInt());
+    }
+
+    private static int generateInt() {
+        return RandomUtils.nextInt();
     }
 
     private static String generateWelcomePhrase() {
