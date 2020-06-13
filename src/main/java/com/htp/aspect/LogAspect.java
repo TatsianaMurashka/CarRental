1325cd5582c2107bf13411193daebecd23b8682f
TatsianaMurashka <murashkotatsiana@gmail.com>
2020-06-13 12:06:15 +0300

    Add constructor to LogAspect

--- src/main/java/com/htp/aspect/LogAspect.java
+++ src/main/java/com/htp/aspect/LogAspect.java
@@ -6,7 +6,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
@@ -16,8 +15,11 @@ public class LogAspect {
 
     private static final Logger log = Logger.getLogger(LogAspect.class);
 
-    @Autowired
     private StopWatch watch;
+
+    public LogAspect(StopWatch watch) {
+        this.watch = watch;
+    }
 
     @Pointcut("execution(* com.htp.dao..*(..))")
     public void aroundRepositoryPointcut() {
